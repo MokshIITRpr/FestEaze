52b71d963c12b17a2a0f6430bee588f659a6d78c
Moksh <mokshkandpal@gmail.com>
2025-01-24 12:07:45 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Messages/messages.dart';
+import 'Pages/login.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +11,25 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent[400],
+          title: Text(
+            welcomeMessage,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+            ),
+          ),
+          centerTitle: true,
+          // TODO(Swayam) : add a leading image or icon here maybe
         ),
+        body: LoginPage(),
       ),
     );
   }
